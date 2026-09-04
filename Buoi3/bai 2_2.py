e6aa7c6f59955071b963a23681df660ab76db366
vuquocchi <chivu2912k6@gmail.com>
2026-09-04 10:54:09 +0700

    Hoan thanh buoi 3

--- Buoi3/bai 2_2.py
+++ Buoi3/bai 2_2.py
@@ -15,3 +15,17 @@ for hang in ma_tran:
     for phan_tu in hang:
         print(phan_tu, end=" ")
     print()
+
+    ma_tran = [
+    [1, 2, 3],
+    [4, 5, 6],
+    [7, 8, 9]
+]
+
+tong = 0
+
+for hang in ma_tran:
+    for phan_tu in hang:
+        tong = tong + phan_tu
+
+print("Tong tat ca phan tu:", tong)
